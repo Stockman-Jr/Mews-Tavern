bb819515320d9ab89e874a3532f8b8ecdfadabac
Stockman-Jr <stockmanmimmi@gmail.com>
2023-02-13 17:18:26 +0000

    Install react router dom and set up first routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import {Navbar, Container, NavLink, Nav} from 'react-bootstrap';
+import {Navbar, Container, Nav} from 'react-bootstrap';
 import styles from '../styles/NavBar.module.css';
 import logo from '../assets/logo.png';
+import { NavLink } from 'react-router-dom';
 
 
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="lg">
-    <Navbar.Brand href="#home"><img className={styles.Logo} src={logo}/></Navbar.Brand>
+    <NavLink to="/">
+    <Navbar.Brand href="#home">
+      <img className={styles.Logo} src={logo}/>
+    </Navbar.Brand>
+    </NavLink>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <NavLink className={styles.NavLink} href="#home">Home</NavLink>
-      <NavLink href="#link">Feed</NavLink>
-      <NavLink href="#link">PokéDex</NavLink>
+      <NavLink className={styles.NavLink} to="/">Home</NavLink>
+      <Nav.Link href="#link">Feed</Nav.Link>
+      <Nav.Link href="#link">PokéDex</Nav.Link>
     </Nav>
     <Nav className={styles.NavRight}>
-      <NavLink className={styles.NavLink} href="#home">Sign Up</NavLink>
-      <NavLink href="#link">Log In</NavLink>
+      <NavLink className={styles.NavLink} to="/signup">Sign Up</NavLink>
+      <NavLink className={styles.NavLink} to="/signin">Log In</NavLink>
     </Nav>
   </Navbar.Collapse>
 </Navbar>

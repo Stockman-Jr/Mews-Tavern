d126ce5447f673c85bb5df9c5102aeabd5ea832b
Stockman-Jr <stockmanmimmi@gmail.com>
2023-03-16 08:56:11 +0000

    Add functions to handle form input changes, add form fields

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -13,15 +13,69 @@ import appStyles from "../../App.module.css";
 import styles from "../../styles/PostCreateEditForm.module.css";
 
 import { Link } from "react-router-dom";
+import Asset from "../../components/Asset";
+import Upload from "../../assets/upload.png";
 
 
 function PostCreateForm() {
   const [postData, setPostData] = useState({
     title: "",
     content: "",
+    game_filter: "",
     image: "",
   });
-  const { title, content, image } = postData;
+  const { title, content, game_filter, image } = postData;
+  const imageInput = useRef(null);
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setPostData({
+        ...postData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
+  const formFields = (
+    <div className="text-center">
+      <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          name="content"
+          value={content}
+          onChange={handleChange}
+        />
+      </Form.Group>
+
+
+      <Button
+        className={`${btnStyles.FormBtn} ${btnStyles.Dark} mt-2`}
+        type="submit"
+      >
+        Share
+      </Button>
+    </div>
+  );
 
   return (
     <div>

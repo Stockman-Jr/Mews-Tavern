ba9abfaf3b98418efbd6542c25f147124f5a2ac0
Stockman-Jr <stockmanmimmi@gmail.com>
2023-03-18 13:42:44 +0000

    Add edit and delete functions to Post.js, add dropdown

--- src/components/DropdownMenus.js
+++ src/components/DropdownMenus.js
@@ -5,8 +5,9 @@ import styles from "../styles/Dropdowns.module.css";
 import { CgMore } from "react-icons/cg";
 import { HiPencilAlt, HiTrash } from "react-icons/hi";
 
-const Dots = React.forwardRef(({ onClick, ref}) => (
+const Dots = React.forwardRef(({ onClick }, ref) => (
     <CgMore 
+    className={styles.Toggle} 
     ref={ref}
     onClick={(e) => {
         e.preventDefault();
@@ -18,17 +19,24 @@ const Dots = React.forwardRef(({ onClick, ref}) => (
 
 export const ConfigDropdown = ({ handleEdit, handleDelete }) => {
     return (
-        <Dropdown>
-            <Dropdown.Toggle as={Dots} />
-            <Dropdown.Menu>
-                <Dropdown.Item>
+        <Dropdown className="ml-auto" drop="left">
+            <Dropdown.Toggle as={Dots} className={styles.Toggle} />
+            <Dropdown.Menu
+                className="text-center"
+                popperConfig={{ strategy: "fixed" }}
+            >
+                <Dropdown.Item
+                    className={styles.DropdownItem}
                     onClick={handleEdit}
                     aria-label="edit"
+                >
                     <HiPencilAlt />
                 </Dropdown.Item>
-                <Dropdown.Item>
+                <Dropdown.Item
+                    className={styles.DropdownItem}
                     onClick={handleDelete}
                     aria-label="delete"
+                >
                     <HiTrash />
                 </Dropdown.Item>
             </Dropdown.Menu>
--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 
+import styles from "../../styles/PostCards.module.css";
 import appStyles from "../../App.module.css";
 
 import { Link, useHistory } from "react-router-dom";
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import Avatar from '../../components/Avatar';
+import { ConfigDropdown } from '../../components/DropdownMenus';
+import { axiosRes } from '../../api/axiosDefaults';
 
 
 const Post = (props) => {
@@ -30,10 +33,35 @@ const Post = (props) => {
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
+    const history = useHistory();
+
+    const handleEdit = () => {
+      history.push(`/posts/${id}/edit`)
+    };
+
+    const handleDelete = async () => {
+      try {
+        await axiosRes.delete(`/posts/post/${id}/`);
+        history.goBack();
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
   return (
     <Card>
-        <Card.Header><Avatar src={profile_image} text={owner} />  <Badge className={appStyles.Badge}>{post_type}</Badge></Card.Header>
+        <Card.Header><Avatar src={profile_image} text={owner} />
+        <div className={styles.Right}>
+        <Badge className={appStyles.Badge}>{post_type}</Badge>
+        {is_owner && postPage && (
+          <ConfigDropdown
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+          />
+        )}
+        </div>  
+        
+        </Card.Header>
         <Link to={`/posts/${id}`}>
                 <Card.Img src={image} alt={title} />
             </Link>

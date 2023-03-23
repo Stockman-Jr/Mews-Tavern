cdd83bdf19592e3bedc47a92b97c1841ecd23db4
Stockman-Jr <stockmanmimmi@gmail.com>
2023-03-23 09:14:59 +0000

    Add arrow up component that scrolls to top of page when clicked

---
+++ src/components/ArrowUp.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import Arrow from "../assets/arrow.png";
+import btnStyles from "../styles/Buttons.module.css";
+
+const ArrowUp = () => {
+    const [showTopBtn, setShowTopBtn] = useState(false);
+  
+    useEffect(() => {
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+          setShowTopBtn(true);
+        } else {
+          setShowTopBtn(false);
+        }
+      });
+    }, []);
+  
+    const handleScrollToTop = () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+  
+    return (
+      <div className={btnStyles.ScrollContainer}>
+        {" "}
+        {showTopBtn && (
+          <img
+            onClick={handleScrollToTop}
+            className={btnStyles.ScrollButton}
+            src={Arrow}
+            alt="scrollToTop"
+          ></img>
+        )}{" "}
+      </div>
+    );
+  };
+  
+  export default ArrowUp;

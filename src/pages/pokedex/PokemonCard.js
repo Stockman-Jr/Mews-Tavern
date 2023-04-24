fff91be89fd4b22bbe58c097226726acd43cf285
Stockman-Jr <stockmanmimmi@gmail.com>
2023-04-24 13:44:38 +0000

    Add pokeball button for catching/releasing pokemons

--- src/pages/pokedex/PokemonCard.js
+++ src/pages/pokedex/PokemonCard.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import styles from "../../styles/PokemonCard.module.css";
 import appStyles from "../../App.module.css";
 import Card from "react-bootstrap/Card";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 import ReactPaginate from 'react-paginate';
 import { getGradientForTypes, capitalizeFirstLetter } from "../../utils/utils";
 import { axiosReq, axiosRes } from "../../api/axiosDefaults";
@@ -28,6 +30,7 @@ function PokemonTypes({ types }) {
 
 const PokemonCard = (props) => {
   const { id, pokemon, setCaughtPokemons } = props;
+  const currentUser = useCurrentUser();
 
 
   const handleCatch = async () => {
@@ -62,38 +65,57 @@ const PokemonCard = (props) => {
   };
 
 
-
-
- 
-
-
+  const pokeBall = (
+    <>
+      <div className={aniStyles.PokeBall}>
+        {id ? (
+          <OverlayTrigger
+            placement="bottom"
+            overlay={<Tooltip>Release?</Tooltip>}
+          >
+            <div className={aniStyles.Caught} onClick={handleRelease}></div>
+          </OverlayTrigger>
+        ) : currentUser ? (
+          <OverlayTrigger
+            placement="bottom"
+            overlay={<Tooltip>Catch!</Tooltip>}
+          >
+            <div className={aniStyles.UnCaught} onClick={handleCatch}></div>
+          </OverlayTrigger>
+        ) : (
+          <OverlayTrigger
+            placement="bottom"
+            overlay={<Tooltip>Log in to save caught pokemons!</Tooltip>}
+          >
+            <div className={aniStyles.UnCaught}></div>
+          </OverlayTrigger>
+        )}
+      </div>
+    </>
+  );
 
   return (
     <>
-              <div className={styles.PokemonContainer}>
-                <Card className={styles.PokemonCard}>
-                  <div className={styles.CardFront}>
-                    <Card.Img
-                      className={styles.PokemonCardImage}
-                      src={pokemon.sprite}
-                      alt={pokemon.name} />
-                    <PokemonTypes types={pokemon.types} />
-                    <Card.Body>
-                      <Card.Title className="text-center">
-                        {capitalizeFirstLetter(pokemon.name)}
-                      </Card.Title>
-
-                      <Card.Text className={styles.TextTypes}>
-                        {pokemon.types.join("/")}
-                      </Card.Text>
-
-                    </Card.Body>
-
-                  </div>
-
-                </Card>
-              </div>
-
+      <div className={styles.PokemonContainer}>
+        <Card className={styles.PokemonCard}>
+          <div className={styles.CardFront}>
+            <Card.Img
+              className={styles.PokemonCardImage}
+              src={pokemon.sprite}
+              alt={pokemon.name} />
+            <PokemonTypes types={pokemon.types} />
+            <Card.Body>
+              <Card.Title className="text-center">
+                {capitalizeFirstLetter(pokemon.name)}
+              </Card.Title>
+              <Card.Text className={styles.TextTypes}>
+                {pokemon.types.join("/")}
+              </Card.Text>
+              {pokeBall}
+            </Card.Body>
+          </div>
+        </Card>
+      </div>
     </>
   );
 }

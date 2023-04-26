d49a58c3d3be8ebcca54923aef443825cbf7d2f5
Stockman-Jr <stockmanmimmi@gmail.com>
2023-04-26 10:20:13 +0000

    Add state variables and useEffect hook to fetch profile data

--- src/pages/profiles/ProfileEditForm.js
+++ src/pages/profiles/ProfileEditForm.js
@@ -21,6 +21,38 @@ import {
 import { axiosReq } from "../../api/axiosDefaults";
 
 const ProfileEditForm = () => {
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+  const { id } = useParams();
+  const history = useHistory();
+
+  const [profileData, setProfileData] = useState({
+    name: "",
+    bio: "",
+    avatar: "",
+  });
+  const { name, bio, avatar } = profileData;
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    const handleMount = async () => {
+      if (currentUser?.profile_id?.toString() === id) {
+        try {
+          const { data } = await axiosReq.get(`/profiles/${id}/`);
+          const { name, bio, avatar } = data;
+          setProfileData({ name, bio, avatar });
+        } catch (err) {
+          console.log(err);
+          history.push("/");
+        }
+      } else {
+        history.push("/");
+      }
+    };
+    handleMount();
+  }, [currentUser, history, id]); 
+
+  
   return (
     <Container className="mt-5">
       <Form className={` ${appStyles.BeigeBg}`}>

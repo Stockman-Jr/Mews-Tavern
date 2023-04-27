d03b0d95c6c1a145ab2312c1fae8cbe4b3a5b69f
Stockman-Jr <stockmanmimmi@gmail.com>
2023-04-27 12:30:11 +0000

    Add handleDelete function to delete and update comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -25,6 +25,27 @@ const Comment = (props) => {
   const [showEditForm, setShowEditForm] = useState(false);
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+
+  
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
+
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {}
+  };
+  
   return (
     <>
       <hr className={styles.CmtHr} />
